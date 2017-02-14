1d88b0e4a1d9ccd2754a75cfa259b347dcfd8011
em <em@emtr0.com>
2017-02-14 11:09:25 -0500

    Adding comments to explain each block, as per the Exercise 3 Study Drills.

--- ex3.py
+++ ex3.py
@@ -1,23 +1,33 @@
+# Prints something.
 print "I will now count my chickens:"
 
+# Prints some things and does maths with some other things.
 print "Hens", 25 + 30 / 6
 print "Roosters", 100 - 25 * 3 % 4
 
+# Prints more things.
 print "Now I will count the eggs:"
 
+# More math. Useful but there's a reason I wasn't a math major.
 print 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
 
+# Printing a question this time.
 print "Is it true that 3 + 2 < 5 - 7?"
 
+# MOAR MATHS
 print 3 + 2 < 5 - 7
 
+# Now it's starting to look like I'm doubting my ability to maths.
 print "What is 3 + 2?", 3 + 2
 print "What is 5 - 7?", 5 - 7
 
+# I'm helping myself talk to myself with the program as an intermediary?
 print "Oh, that's why it's False."
 
+# Printing about printing/computing some more.
 print "How about some more."
 
+# Triple checking the maths.
 print "Is it greater?", 5 > -2
 print "Is it greater or equal?", 5 >= -2
 print "Is it less or equal?", 5 <= -2
