cf1ed97f0cfc5e89a36c700d9e1f83d2bc1d7f8b
em <em@emtr0.com>
2017-02-14 11:21:10 -0500

    Updated to use floating point nums per Ex 3 Study Drills.

--- ex3.py
+++ ex3.py
@@ -2,24 +2,24 @@
 print "I will now count my chickens:"
 
 # Prints some things and does maths with some other things.
-print "Hens", 25 + 30 / 6
-print "Roosters", 100 - 25 * 3 % 4
+print "Hens", 25.0 + 30.0 / 6.0
+print "Roosters", 100.0 - 25.0 * 3.0 % 4.0
 
 # Prints more things.
 print "Now I will count the eggs:"
 
 # More math. Useful but there's a reason I wasn't a math major.
-print 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
+print 3.0 + 2.0 + 1.0 - 5.0 + 4.0 % 2.0 - 1.0 / 4.0 + 6.0
 
 # Printing a question this time.
-print "Is it true that 3 + 2 < 5 - 7?"
+print "Is it true that 3.0 + 2.0 < 5.0 - 7.0?"
 
 # MOAR MATHS
-print 3 + 2 < 5 - 7
+print 3.0 + 2.0 < 5.0 - 7.0
 
 # Now it's starting to look like I'm doubting my ability to maths.
-print "What is 3 + 2?", 3 + 2
-print "What is 5 - 7?", 5 - 7
+print "What is 3.0 + 2.0?", 3.0 + 2.0
+print "What is 5.0 - 7.0?", 5.0 - 7.0
 
 # I'm helping myself talk to myself with the program as an intermediary?
 print "Oh, that's why it's False."
@@ -28,6 +28,6 @@ print "Oh, that's why it's False."
 print "How about some more."
 
 # Triple checking the maths.
-print "Is it greater?", 5 > -2
-print "Is it greater or equal?", 5 >= -2
-print "Is it less or equal?", 5 <= -2
+print "Is it greater?", 5.0 > -2.0
+print "Is it greater or equal?", 5.0 >= -2.0
+print "Is it less or equal?", 5.0 <= -2.0
