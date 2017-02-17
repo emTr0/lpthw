c917e007f41d643e85a8a49053e009906abfa42a
emTr0 <em@emtr0.com>
2017-02-16 22:43:12 -0800

    Updating ex4.py per Exercise 4 Study Drills.

--- ex4.py
+++ ex4.py
@@ -1,12 +1,21 @@
+# Set var to int.
 cars = 100
+# Set var to int.
 space_in_a_car = 4.0
+# Set var to int.
 drivers = 30
+# Set var to int.
 passengers = 90
+# Set var to diff of vars.
 cars_not_driven = cars - drivers
+# Set var to var val.
 cars_driven = drivers
+# Set var to val of multiplied vars.
 carpool_capacity = cars_driven * space_in_a_car
+# Set var to result of var division.
 average_passengers_per_car = passengers / cars_driven
 
+# Print a lot of stuff.
 print "There are", cars, "cars available."
 print "There are only", drivers, "drivers available."
 print "There will be", cars_not_driven, "empty cars today."
